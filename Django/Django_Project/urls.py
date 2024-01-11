cae75887053917294a6d5b9e92fef2bbb47ffe5b
MartinaL2005 <martina.lopez@bcestudent.com>
2024-01-11 13:59:09 +0000

    Save all

--- Django/Django_Project/urls.py
+++ Django/Django_Project/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from BeanApp.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", index),
 ]
